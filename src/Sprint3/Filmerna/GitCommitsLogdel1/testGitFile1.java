e17ea31dd039ca53e3bf41e1a46a78c742ef6bbf
Filip Detterfelt <fillip.detterfelt@gmail.com>
2023-10-26 14:34:12 +0200

    branch demo skriv ut hejsan svejsan

--- src/Sprint3/Filmerna/GitCommitsLogdel1/testGitFile1.java
+++ src/Sprint3/Filmerna/GitCommitsLogdel1/testGitFile1.java
@@ -2,4 +2,7 @@ package Sprint3.Filmerna.GitCommitsLogdel1;
 
 public class testGitFile1 {
     //test file
+    public static void main(String[] args) {
+        System.out.println("HEjsan svejan ");
+    }
 }
